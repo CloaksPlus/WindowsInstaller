5185e796aa1db63cfe254a7421936e3f27f110ae
Wowkster <adyowowk@gmail.com>
2021-11-02 03:10:04 -0400

    Fix Dependency Resolution

--- Cloaks/App.xaml.cs
+++ Cloaks/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
 using System.Windows.Media;
 
 namespace Cloaks
@@ -7,8 +11,53 @@ namespace Cloaks
     {
         static App()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
+
             TextOptions.TextFormattingModeProperty.OverrideMetadata(typeof(Window),
                 new FrameworkPropertyMetadata(TextFormattingMode.Display, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.Inherits));
+        }
+
+        private static Assembly OnResolveAssembly(object sender, ResolveEventArgs e)
+        {
+            var thisAssembly = Assembly.GetExecutingAssembly();
+
+            // Get the Name of the AssemblyFile
+            var assemblyName = new AssemblyName(e.Name);
+            var dllName = assemblyName.Name + ".dll";
+
+            // Load from Embedded Resources - This function is not called if the Assembly is already
+            // in the same folder as the app.
+            var resources = thisAssembly.GetManifestResourceNames().Where(s => s.EndsWith(dllName));
+            if (resources.Any())
+            {
+
+                // 99% of cases will only have one matching item, but if you don't,
+                // you will have to change the logic to handle those cases.
+                var resourceName = resources.First();
+                using (var stream = thisAssembly.GetManifestResourceStream(resourceName))
+                {
+                    if (stream == null) return null;
+                    var block = new byte[stream.Length];
+
+                    // Safely try to load the assembly.
+                    try
+                    {
+                        stream.Read(block, 0, block.Length);
+                        return Assembly.Load(block);
+                    }
+                    catch (IOException)
+                    {
+                        return null;
+                    }
+                    catch (BadImageFormatException)
+                    {
+                        return null;
+                    }
+                }
+            }
+
+            // in the case the resource doesn't exist, return null.
+            return null;
         }
     }
 }
